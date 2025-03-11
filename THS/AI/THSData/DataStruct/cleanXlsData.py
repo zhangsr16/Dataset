60d5212bfa4098f966795c05123819ef4ed9a0de
zhangsr16 <145007394+zhangsr16@users.noreply.github.com>
2025-03-11 20:30:04 +0800

    Update cleanXlsData.py

--- THS/AI/THSData/DataStruct/cleanXlsData.py
+++ THS/AI/THSData/DataStruct/cleanXlsData.py
@@ -1,34 +1,41 @@
 import os
 import pandas as pd
 
-
 def replace_errors_in_excel(file_path, replacements):
-    try:
-        # 读取xls文件
-        df = pd.read_excel(file_path, engine='openpyxl', dtype=str)
-        # 替换错误字符
-        for error_char, replacement_char in replacements.items():
-            df = df.map(lambda x: x.replace(error_char, replacement_char) if isinstance(x, str) else x)
-        # 删除原xls文件
-        os.remove(file_path)
-        # 保存为xlsx文件
-        df.to_excel(file_path, index=False)
+    # 读取xls文件
+    df = pd.read_excel(file_path, engine='openpyxl', dtype=str)
+    # 替换错误字符
+    for error_char, replacement_char in replacements.items():
+        df = df.applymap(lambda x: x.replace(error_char, replacement_char) if isinstance(x, str) else x)
+    # 尝试将所有列转换为数值类型
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors='ignore')
+    # 删除原xls文件
+    os.remove(file_path)
+    # 保存为xlsx文件
+    df.to_excel(file_path, index=False)
+
+def trans_summary_dataframe(file_path):
+    df = pd.read_excel(file_path, engine='openpyxl')
+    # 将 'index_column' 列设置为新的行索引
+    df.set_index('单日情况', inplace=True)
+    # 转置 DataFrame
+    df_transposed = df.transpose()
+    # 重置索引以便查看
+    df_transposed.reset_index(inplace=True)
+    # 获取新的列名
+    new_columns = df_transposed.columns.tolist()
+    new_columns[0] = '版块'
+    df_transposed.columns = new_columns
+    # 删除原xls文件
+    os.remove(file_path)
+    # 保存为xlsx文件
+    df_transposed.to_excel(file_path, index=False)
 
 
-    except Exception as e:
-        print(f"Failed to process {file_path}: {e}")
-
-
-def batch_process_xls(directory, replacements):
-    # 遍历目录中的所有文件
-    for filename in os.listdir(directory):
-        if filename.endswith('.xlsx'):
-            file_path = os.path.join(directory, filename)
-            replace_errors_in_excel(file_path, replacements)
-
-
-# 设置要处理的目录和要替换的字符
-directory = "F:/Desktop/THS/data/test2"  # 替换为你的目录路径
+# 设置要处理的目录
+directory = "./"  # 替换为你的目录路径
+# 替换
 replacements = {
     "%": "",
     "↑": "",
@@ -36,6 +43,9 @@ replacements = {
     "-": "",
     "亏损": "-1",
     "负资产": "-1",
-}  # 替换为你要替换的错误字符和正确字符的对应关系
-
-batch_process_xls(directory, replacements)
+}
+for filename in os.listdir(directory):
+    if filename.endswith('.xlsx'):
+        file_path = os.path.join(directory, filename)
+        trans_summary_dataframe(file_path)
+        #replace_errors_in_excel(file_path, replacements)
